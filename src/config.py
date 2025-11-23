3aab013cec6a06f1cdefbee60677868b502cee72
Raghul <raghulchandramouli@gmail.com>
2025-11-23 17:30:20 +0530

    added configuration between real & Fake Images

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SAMPLE_IMAGES_DIR = os.path.join(BASE_DIR, 'sample_images')
+
+# Images
+REAL_DIR = os.path.join(SAMPLE_IMAGES_DIR, 'real')
+FAKE_DIR = os.path.join(SAMPLE_IMAGES_DIR, 'fake')
